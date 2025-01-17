b4ad7e7ad57fe4d5932d8c8f94fe9c4c365c77a8
stavan54sk <stavan54sk@gmail.com>
2025-01-17 10:12:55 +0530

    added code till pattern 8

--- src/main/java/com/pattern/Patterns.java
+++ src/main/java/com/pattern/Patterns.java
@@ -4,21 +4,96 @@ package com.pattern;
  * Hello world!
  *
  */
-public class Patterns 
-{
-	
-	void patterns1(int n){
-		
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			System.out.print("*");
+public class Patterns {
+
+	void patterns1(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
-		
+
+	void patterns2(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
 	}
-    public static void main( String[] args )
-    {
-        new Patterns().patterns1(5);
-    }
+	
+	void patterns3(int n) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+	
+	void patterns4(int n) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+	
+	void patterns5(int n) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+	
+	void patterns6(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <(n-i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+	
+	void patterns7(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <(n-i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+	
+	void patterns8(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <=(n-i); j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new Patterns().patterns8(5);
+	}
 }
