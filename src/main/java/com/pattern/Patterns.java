e9e9c01c67aed8157c7320cd5166fd7dbcc1c464
stavan54sk <stavan54sk@gmail.com>
2025-01-21 10:43:03 +0530

    updated pattern till pattern 20

--- src/main/java/com/pattern/Patterns.java
+++ src/main/java/com/pattern/Patterns.java
@@ -159,22 +159,111 @@ public class Patterns {
 	}
 
 	void patterns13(int n) {
-		int v=1;
+		int v = 1;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(v);
-				v=1-v;
+				v = 1 - v;
 			}
 			System.out.println();
 		}
 
 	}
-	
+
 	void patterns14(int n) {
-		int v=1;
+
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(v+" ");
+				System.out.print(j);
+			}
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+	void patterns15(int n) {
+		int v = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(v + " ");
+				v++;
+			}
+			System.out.println();
+		}
+
+	}
+
+	void patterns16(int n) {
+		for (int i = 1; i <= n; i++) {
+			char v = 65;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(v);
+				v++;
+			}
+			System.out.println();
+		}
+
+	}
+
+	void patterns17(int n) {
+		for (int i = 0; i < n; i++) {
+			char v = 65;
+			for (int j = 0; j < (n - i); j++) {
+				System.out.print(v);
+				v++;
+			}
+			System.out.println();
+		}
+
+	}
+
+	void patterns18(int n) {
+		char v = 64;
+		for (int i = 1; i <= n; i++) {
+			v++;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(v);
+			}
+			System.out.println();
+		}
+
+	}
+
+	void patterns19(int n) {
+		for (int i = 0; i < n; i++) {
+			char v = 65;
+//			space
+			for (int j = 1; j < (n - i); j++) {
+				System.out.print(" ");
+			}
+
+//			char
+			for (int j = 0; j < (2 * i) + 1; j++) {
+				System.out.print(v);
+				if (j < i) {
+					v++;
+				}
+				else {
+					v--;
+				}
+			}
+
+			System.out.println();
+		}
+
+	}
+	
+	void patterns20(int n) {
+		for (int i = 1; i <= n; i++) {
+			char v = (char) (65+n-i+1);
+			for (int j = 1; j <= i; j++) {
+				System.out.print(v);
 				v++;
 			}
 			System.out.println();
@@ -183,6 +272,6 @@ public class Patterns {
 	}
 
 	public static void main(String[] args) {
-		new Patterns().patterns14(5);
+		new Patterns().patterns20(20);
 	}
 }
